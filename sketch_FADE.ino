7691ae00f5d2999e67a42883999c39e0c6eaa0fc
JenMen <kirinbolt7@gmail.com>
2016-04-03 20:47:39 -0400

    Added files via upload
    Sketch for 3 LEDs

---
+++ sketch_FADE.ino
@@ -0,0 +1,46 @@
+// 3 LED Fade in and out. 4/3/2016 Spring
+int led1 = 3;
+int led2 = 10;
+int led3 = 11;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+Serial.begin(9600);
+pinMode(led1,OUTPUT);
+pinMode(led2,OUTPUT);
+pinMode(led3,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
+for(int i = 0;i < 255; i++){
+  Serial.println(i);
+  analogWrite(led1,i);
+  delay(2);
+  analogWrite(led2,i);
+  delay(2);
+  analogWrite(led3,i);
+  delay(2);
+}
+
+for(int i = 255; i > 0; i--){
+  Serial.println(i);
+  analogWrite(led1,i);
+  delay(2);
+  analogWrite(led2,i);
+  delay(2);
+  analogWrite(led3,i);
+  delay(2);
+  
+
+  
+  
+}
+
+
+}
